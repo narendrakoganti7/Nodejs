ab396b7eb3d77982f16e3e2c65a623f81ba267af
kubesys <narendra.devops@outlook.co>
2019-12-03 13:49:42 +0800

    Restfull API

---
+++ express-fram/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const products = [
+    {id: 1, product: 'mobiles'},
+    {id: 2, product: 'TVs'},
+    {id: 3, product: 'toys'}
+];
+
+app.get('/',(req,res) => {
+res.send('Hello World12');
+});
+
+app.get('/imge',(req,res) => {
+    res.send([1,2,3]);
+}
+);
+app.get('/product/:id', (req,res) => {
+    const product = products.find(p => p.id === parseInt(req.params.id));
+    if (!product) res.status(404).send('There is no product in the list');
+    res.send(product);
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => console.log(`listening port ${port} ....`)); 
